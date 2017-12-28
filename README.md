4d80afd9f4760735b3a2d56e5c25eca8f17e906d
Tiago Antunes <tiagoantunes14@gmail.com>
2017-12-28 18:08:01 -0400

    added dump of my database

---
+++ README.md
@@ -0,0 +1,23 @@
+Irei descrever o processo de criação em passos.
+
+Passo 1 - Foi instalado Apache como servidor e banco de dados mysql, alem de outras necessidades como PHP e bibliotecas.
+
+Passo 2 - Foi criado os arquivos para inserir, remover e selecionar dados do banco dados. Todos estes se encontram neste
+repositorio.
+
+Passo 3 - Foi criado apenas uma tela principal, para inserir, deletar e mostrar os produtos que estão presentes no 
+banco de dados.
+  No processo de criação da tela foi usado bootstrap, e para conversar com os scripts em php foi utilizado os metodos HTTP 
+get and Post, utilizando objetos json.
+
+A tela é bem intuitiva, apenas clicar nos botões.
+
+![alt text](http://url/to/img.png)
+
+Neste repositorio existe tbm o dump do banco, caso seja necessário. 
+
+Qualquer dúvidas entrar em contato. 
+
+
+
+
---
+++ fpf-dump.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: FPF_Produtos
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` varchar(45) NOT NULL,
+  `descricao_produto` varchar(140) DEFAULT NULL,
+  `data_produto` varchar(100) NOT NULL,
+  `imagem_produto` longblob,
+  `preco_produto` float NOT NULL DEFAULT '0',
+  `origem_produto` varchar(45) DEFAULT NULL,
+  `categoria_produto` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'Limao',NULL,'2017-12-24 16:57:04',NULL,4,NULL,NULL),(2,'asd',NULL,'12/12/1992',NULL,2,NULL,NULL),(3,'soda',NULL,'12.12\"',NULL,4,NULL,NULL),(4,'massa',NULL,'18-19',NULL,3,NULL,NULL),(5,'teste',NULL,'123',NULL,0,NULL,NULL),(14,'qwe',NULL,'gdf',NULL,0,NULL,NULL),(15,'final teste',NULL,'final teste',NULL,0,NULL,NULL);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-28 18:06:21
